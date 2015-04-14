e3263d64c60d0cee8fb22243a9ae472a54490510
yangguangcqq <915545741@qq.com>
2015-04-14 09:43:10 +0800

    hello word

---
+++ z/src/test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Administrator
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("hello word!");
+
+	}
+
+}
